625323173bdaf486b2a0f34ba2cd3f0f25007544
hahava <hahava5696@gmail.com>
2021-02-08 20:22:47 +0900

    Initial Commit

---
+++ my-calculator/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  my-calculator
+//
+//  Created by hahava on 2021/02/08.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
