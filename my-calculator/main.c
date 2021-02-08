cccb241890ef2762617d8ad393bf41e3acc075f1
hahava <hahava5696@gmail.com>
2021-02-08 20:38:08 +0900

    Add calculator string template

--- my-calculator/main.c
+++ my-calculator/main.c
@@ -1,14 +1,27 @@
-//
-//  main.c
-//  my-calculator
-//
-//  Created by hahava on 2021/02/08.
-//
-
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+void printCalculator(char *input);
+
+int main()
+{
+    char *input = "1234";
+    printCalculator(input);
     return 0;
+}
+
+void printCalculator(char *input)
+{
+    printf("|  _________________  |\n");
+    printf("| |%17s| |\n", input);
+    printf("| |_________________| |\n");
+    printf("|  ___ ___ ___   ___  |\n");
+    printf("| | 7 | 8 | 9 | | + | |\n");
+    printf("| |___|___|___| |___| |\n");
+    printf("| | 4 | 5 | 6 | | - | |\n");
+    printf("| |___|___|___| |___| |\n");
+    printf("| | 1 | 2 | 3 | | x | |\n");
+    printf("| |___|___|___| |___| |\n");
+    printf("| | . | 0 | = | | / | |\n");
+    printf("| |___|___|___| |___| |\n");
+    printf("|_____________________|\n");
 }
